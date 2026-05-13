3d9adebc672901ab12f8d3eb7f7224f210ed635b
novakpetr1985 <novakpetr1985@gmail.com>
2026-05-13 15:21:22 +0200

    Add materials
    s-heat
    s-joint
    s-flex
    s-safe
    s-mat

--- app/main.py
+++ app/main.py
@@ -1,7 +1,36 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI(title="Warehouse Traceability API")
+
+
+# Fake databáze
+materials = []
+
+
+# Datový model
+class Material(BaseModel):
+    name: str
+    quantity: int
+    note: str
+
 
 @app.get("/")
 async def root():
     return {"status": "running"}
+
+
+# CREATE
+@app.post("/materials")
+async def create_material(material: Material):
+    materials.append(material)
+    return {
+        "message": "Material created",
+        "data": material
+    }
+
+
+# READ
+@app.get("/materials")
+async def get_materials():
+    return materials
